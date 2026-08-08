ad1f8cd5af893d27e5ec43e2a475176939dacbf1
Paulwin <paulwin294@gmail.com>
2026-08-08 18:56:10 +0530

    Before admin panel

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig({
-  plugins: [
-    tanstackStart({
-      customViteReactPlugin: true,
-    }),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [tanstackStart({
+    customViteReactPlugin: true,
+  }), react(), tailwindcss(), cloudflare({
+    viteEnvironment: {
+      name: "ssr"
+    }
+  })],
   resolve: {
     tsconfigPaths: true,
   },
